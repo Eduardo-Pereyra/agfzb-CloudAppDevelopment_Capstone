96290c83a4ec6b761a1b3ea93727d26a17948a55
Muhammad Azhar <azharskp1994@gmail.com>
2023-07-11 13:17:34 +0500

    Initial commit

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -1,33 +1,30 @@
-"""IBM Cloud Function that gets all reviews for a dealership
-
-Returns:
-    List: List of reviews for the given dealership
-"""
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
 from cloudant.client import Cloudant
 from cloudant.error import CloudantException
 import requests
 
 
-def main(param_dict):
-    """Main Function
-
-    Args:
-        param_dict (Dict): input paramater
-
-    Returns:
-        _type_: _description_ TODO
-    """
+def main(dict):
+    databaseName = "dealerships"
 
     try:
         client = Cloudant.iam(
-            account_name=param_dict["COUCH_USERNAME"],
-            api_key=param_dict["IAM_API_KEY"],
+            account_name=dict["COUCH_USERNAME"],
+            api_key=dict["IAM_API_KEY"],
             connect=True,
         )
-        print(f"Databases: {client.all_dbs()}")
-    except CloudantException as cloudant_exception:
+        print("Databases: {0}".format(client.all_dbs()))
+    except CloudantException as ce:
         print("unable to connect")
-        return {"error": cloudant_exception}
+        return {"error": ce}
     except (requests.exceptions.RequestException, ConnectionResetError) as err:
         print("connection error")
         return {"error": err}
